b1107f5d9eeb6fa079c9b007a9a1ac06ea157657
David.Rz.Liu <mania7539@gmail.com>
2020-01-23 18:32:58 +0800

    lec54. Display Profiles

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
+import Profiles from './components/profiles/Profiles';
 
 import './App.css';
 
@@ -47,6 +48,12 @@ const App = () => {
                                 component={Register}
                             />
                             <Route exact path='/login' component={Login} />
+                            <Route
+                                exact
+                                path='/profiles'
+                                component={Profiles}
+                            />
+                            # /profiles will not be a private route
                             <PrivateRoute
                                 exact
                                 path='/dashboard'
