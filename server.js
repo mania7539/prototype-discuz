1f74da03bb12c83f5c13c5322734af21cf84575b
David.Rz.Liu <mania7539@gmail.com>
2020-01-31 22:27:01 +0800

    lec70. Prepare For Deployment

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ connectDB();
 // Initialize Middleware (bodyParser)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// app.get('/', (req, res) => res.send('API Running'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
