563070b196c2ffb35e62bde48533f8a0136c42c9
David.Rz.Liu <mania7539@gmail.com>
2020-02-02 16:22:23 +0800

    fixing: internal server error on heroku when logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
                 </Link>
             </li>
             <li>
-                <a onClick={logout} href='/login'>
+                <a onClick={logout} href='/'>
                     <i className='fas fa-sign-out-alt'></i>{' '}
                     <span className='hide-sm'>Logout</span>
                 </a>
