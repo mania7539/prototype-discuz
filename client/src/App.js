a50bdcd51f6b242fd585cea4e9dc27b58962527c
David.Rz.Liu <mania7539@gmail.com>
2020-02-02 17:43:40 +0800

    lec73. Not Found Page & Theme Workaround (cont. v2)

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
                     <Navbar />
                     <Switch>
                         <Route exact path='/' component={Landing} />
-                        <Route component={Routes} />
+                        <Route path='*' component={Routes} />
                     </Switch>
                     {/*-- validate above code (return body=v01), with browser: http://localhost:3000 
                         /
